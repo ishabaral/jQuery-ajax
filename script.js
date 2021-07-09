24555c8c1a64ded6a5038785d7d7e27c4a12f8c6
ishabaral <baralisha.16@gmail.com>
2021-07-09 12:18:40 +0545

    added search button with filter function

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 $(document).ready(function () {
 
     $("#load-data-button").click(function () {
+        var searchBox = $("<input type = 'text' placeholder = 'Search'>")
+        $("#load-data-button").after(searchBox);
         var table = $("<table></table>");
-        $("#load-data-button").after(table);
+        $("input").after(table);
         $("table").append('<tr><th> Id </th><th> UserId </th><th> Title </th><th> Body </th></tr>');
         
         $.get("https://jsonplaceholder.typicode.com/posts", (data, status)=>{
             for(let i=0; i< data.length; i++){
                 $("table").append('<tr><td>'+ data[i].id + '</td><td>'+ data[i].userId + '</td><td>' + data[i].title + '</td><td>' + data[i].body+ '</td></tr>' );
             }
+        });
+
+        $("input").keyup(function () {
+            var value = $(this).val().toLowerCase();
+            $("table tr").filter(function () {
+                var tableTitle = $("tr:first-child");
+                $(this).toggle( $(this).text().toLowerCase().indexOf(value) > -1);
+            })
         })
+
     })
 });
