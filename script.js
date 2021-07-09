85b96663ed8946ff07b1aec69386ae5ec209208a
ishabaral <baralisha.16@gmail.com>
2021-07-09 09:57:06 +0545

    added css

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $("#load-data-button").click(function () {
         var table = $("<table></table>");
         $("#load-data-button").after(table);
-        $("table").append('<tr><th> Id </th><th> userId </th><th> Title </th><th> Body </th></tr>');
+        $("table").append('<tr><th> Id </th><th> UserId </th><th> Title </th><th> Body </th></tr>');
         
         $.get("https://jsonplaceholder.typicode.com/posts", (data, status)=>{
             for(let i=0; i< data.length; i++){
