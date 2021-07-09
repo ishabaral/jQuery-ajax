228034700e215a25bbbf7c3121bd0e9dc043212a
ishabaral <baralisha.16@gmail.com>
2021-07-09 09:37:35 +0545

    load data from api into table

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $("#load-data-button").click(function () {
+        var table = $("<table></table>");
+        $("#load-data-button").after(table);
+        $("table").append('<tr><th> Id </th><th> userId </th><th> Title </th><th> Body </th></tr>');
+        
+        $.get("https://jsonplaceholder.typicode.com/posts", (data, status)=>{
+            for(let i=0; i< data.length; i++){
+                $("table").append('<tr><td>'+ data[i].id + '</td><td>'+ data[i].userId + '</td><td>' + data[i].title + '</td><td>' + data[i].body+ '</td></tr>' );
+            }
+        })
+    })
+});
